da7fd310a608d4e1b9c838d602fd67c554375712
Yevgeniy Kap <kapustaevg@gmail.com>
2023-12-10 18:08:55 +0300

    change time output

--- src/services/time.py
+++ src/services/time.py
@@ -13,22 +13,21 @@ class Time(object):
     def get_unix_time(self) -> int:
         return self.__unix_date
 
-    def get_text_time(self) -> datetime:
+    def get_text_time(self) -> str:
         return self.__date
 
     def __convert_str_time_to_int(self) -> int:
         now: datetime = datetime.now(timezone('Europe/Moscow'))
-        date: datetime = (
+        date: datetime = timezone('Europe/Moscow').localize(
             datetime.strptime(self.__user_time_input, "%d %H:%M").
             replace(year=now.year).
             replace(month=now.month)
         )
-
         if date < now:
             if date.month < 12:
                 date: datetime = date.replace(month=date.month + 1)
             else:
                 date: datetime = date.replace(year=date.year + 1)
                 date: datetime = date.replace(month=date.month - 11)
-        self.__date: datetime = date
+        self.__date: str = date.strftime("%m.%d %H:%M")
         return int(date.timestamp())
